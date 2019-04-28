392dbedc11145fd12ef232d2c5290564a65bade6
kishan <kishand.261194@gmail.com>
2019-04-27 20:13:29 -0400

    added login

--- ASK-app/routes/index.js
+++ ASK-app/routes/index.js
@@ -5,7 +5,96 @@ const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 const mongoose = require('mongoose');
 const Flights = mongoose.model('Flight');
 
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+// Load User model
+const User = require('../models/User');
+
+let login = (req, res) => res.render('login');
+
 let index = (req, res) => res.render('index');
+
+let register = (req, res) => res.render('register');
+
+let addUser = (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
+
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: 'Please enter all fields' });
+  }
+
+  if (password != password2) {
+    errors.push({ msg: 'Passwords do not match' });
+  }
+
+  if (password.length < 6) {
+    errors.push({ msg: 'Password must be at least 6 characters' });
+  }
+
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    });
+  } else {
+    User.findOne({ email: email }).then(user => {
+      if (user) {
+        errors.push({ msg: 'Email already exists' });
+        res.render('register', {
+          errors,
+          name,
+          email,
+          password,
+          password2
+        });
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => {
+                req.flash(
+                  'success_msg',
+                  'You are now registered and can log in'
+                );
+                res.redirect('/users/login');
+              })
+              .catch(err => console.log(err));
+          });
+        });
+      }
+    });
+  }
+};
+
+
+
+let loginRequest = (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/flights',
+    failureRedirect: '/users/login',
+    failureFlash: true
+  })(req, res, next);
+};
+
+let logout = (req, res) => {
+  req.logout();
+  req.flash('success_msg', 'You are logged out');
+  res.redirect('/users/login');
+};
 
 let flights = (req, res) => {
   Flights.find().then( docs => {
@@ -53,13 +142,19 @@ let update = (req, res) => {
   res.redirect('/flights');
 };
 
-  app.get('/', index);
-  app.get('/flights', flights);
-  app.get('/add', add);
-  app.get('/edit/:flightNumber', edit);
+
+  app.get('/users/login', forwardAuthenticated, login);
+  app.get('/users/register', forwardAuthenticated, register);
+  app.get('/users/logout',ensureAuthenticated,logout)
+  app.get('/', ensureAuthenticated, index);
+  app.get('/flights', ensureAuthenticated,flights);
+  app.get('/add', ensureAuthenticated,add);
+  app.get('/edit/:flightNumber', ensureAuthenticated,edit);
   app.get('*', notFound);
-  app.post('/save', save);
-  app.post('/update/:flightNumber', update);
-  app.delete('/delete/:flightNumber', remove);
+  app.post('/save', ensureAuthenticated,save);
+  app.post('/users/register', ensureAuthenticated,addUser);
+  app.post('/users/login', loginRequest);
+  app.post('/update/:flightNumber', ensureAuthenticated,update);
+  app.delete('/delete/:flightNumber', ensureAuthenticated,remove);
 
 }; // Finish exports
--- ASK-app/server.js
+++ ASK-app/server.js
@@ -1,7 +1,10 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+const expressLayouts = require('express-ejs-layouts');
 // App
 const path = require('path');
 const app = express();
@@ -13,10 +16,37 @@ app.use(bodyParser.json());
 app.use('/public', express.static('public'));
 app.use(express.static(path.join(__dirname, '../ASK-blockchain/build/contracts')));
 // Configuration settings
+app.use(expressLayouts);
 app.set('view engine', 'ejs');
 // Variables
 const port = 3000;
 // Routes
+
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
 const routes = require('./routes')(app);
 
 app.listen(port, err => console.log( err ? `An error has ocurred! ${err}` : `App running on port ${port}` ) );
